e8b80a0cfa5857d9e2f48e6146a9ecf08ff27e27
Lungsdude <82831388+lungsdude@users.noreply.github.com>
2024-11-09 23:40:20 +0200

    Update test_package/conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,18 +1,25 @@
 import os
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 
-
-class LiblavaTestConan(ConanFile):
+class LibLavaTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep, run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
