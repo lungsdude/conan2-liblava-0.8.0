ad5dfe5f9cbd7b449bb35b4bfbcc085846afc2ec
Lungsdude <82831388+lungsdude@users.noreply.github.com>
2024-11-09 23:39:45 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,74 +1,65 @@
 import os
 import shutil
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy, rmdir
+from conan.tools.build import check_min_cppstd
+from conan.tools.scm import Git
 
 class LiblavaConan(ConanFile):
     name = "liblava"
-    version = "0.7.3"
+    version = "0.8.0"
     license = "MIT"
     author = "Lava Block OÜ (code@liblava.dev)"
-    url = "https://liblava.dev"
+    url = "https://github.com/liblava/liblava.git"
     description = "A modern and easy-to-use library for the Vulkan API"
     topics = ("vulkan", "graphics", "rendering")
     settings = "os", "compiler", "build_type", "arch"
-    # TODO shared library build
-    # requires changes to liblava CMakeLists.txt and settings for subrepos
     options = {
         "fPIC": [True, False],
         "test": [True, False],
-        "demo": [True, False],
-        #"shared": [True, False]
+        "demo": [True, False]
     }
     default_options = {
         "fPIC": True,
         "test": False,
-        "demo": False,
-        #"shared": False
+        "demo": False
     }
-    generators = "cmake"
-    no_copy_source=True
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON" if self.options.get_safe("fPIC") else "OFF"
-        cmake.definitions["LIBLAVA_TEST"] = "ON" if self.options.get_safe("test") else "OFF"
-        cmake.definitions["LIBLAVA_DEMO"] = "ON" if self.options.get_safe("demo") else "OFF"
-        cmake.definitions["LIBLAVA_TEMPLATE"] = "OFF"
-        cmake.configure()
-        return cmake
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "LICENSE"
+    no_copy_source = True
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-    
+
     def configure(self):
-        # Check if the current profile config supports C++20
-        tools.check_min_cppstd(self, "20")
+        check_min_cppstd(self, "20")
 
     def source(self):
-        git = tools.Git()
-        git.clone(self.url)
-        git.checkout(self.version)
+        git = Git(self)
+        git.clone(self.url, self.source_folder, args=["-b", self.version])
 
-        # guarantee proper /MT /MD linkage in MSVC
-        project_line = "project(liblava VERSION {} LANGUAGES C CXX)".format(self.version)
-        tools.replace_in_file("CMakeLists.txt", project_line,
-                              '''
-                              {}
-                              include(${{CMAKE_BINARY_DIR}}/conanbuildinfo.cmake)
-                              conan_basic_setup()
-                              '''
-                              .format(project_line))
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON" if self.options.get_safe("fPIC") else "OFF"
+        tc.variables["LIBLAVA_TEST"] = "ON" if self.options.get_safe("test") else "OFF"
+        tc.variables["LIBLAVA_DEMO"] = "ON" if self.options.get_safe("demo") else "OFF"
+        tc.variables["LIBLAVA_TEMPLATE"] = "OFF"
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        shutil.rmtree(os.path.join(self.package_folder, "lib/cmake"), ignore_errors=True)
-        self.copy("LICENSE", dst="licenses", src=self.source_folder)
+        rmdir(self, os.path.join(self.package_folder, "lib/cmake"))
+        copy(self, "LICENSE", dst="licenses", src=self.source_folder)
 
     def package_info(self):
         self.cpp_info.includedirs = [
@@ -78,11 +69,9 @@ class LiblavaConan(ConanFile):
         self.cpp_info.libdirs = ["lib"]
         self.cpp_info.bindirs = ["bin"]
 
-        self.cpp_info.cxxflags += [tools.cppstd_flag(self.settings)]
         self.cpp_info.defines += ["SPDLOG_COMPILED_LIB"]
 
         self.cpp_info.libs = [
-            # in correct linking order
             "lava.engine",
             "lava.app",
             "lava.block",
@@ -95,17 +84,15 @@ class LiblavaConan(ConanFile):
             "lava.core",
             "shaderc_combined",
             "glfw3",
-            "physfs" if self.settings.compiler != "Visual Studio" else "physfs-static",
+            "physfs" if self.settings.compiler != "msvc" else "physfs-static",
             "spdlog"
         ]
 
-        # std::filesystem library
         if self.settings.get_safe("compiler.libcxx") == "libstdc++11":
             self.cpp_info.system_libs += ["stdc++fs"]
 
         if self.settings.get_safe("compiler") in ["gcc", "clang"]:
             thread_flag = "-pthread"
-            self.cpp_info.cxxflags        += [thread_flag]
-            self.cpp_info.sharedlinkflags += [thread_flag]
-            self.cpp_info.exelinkflags    += [thread_flag]
+            self.cpp_info.sharedlinkflags.append(thread_flag)
+            self.cpp_info.exelinkflags.append(thread_flag)
             self.cpp_info.system_libs += ["dl"]
